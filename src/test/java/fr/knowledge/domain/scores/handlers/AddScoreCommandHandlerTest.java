d390f1e7c12776cb9752bac1e9d26667dd4fcb1d
Cyril Pham-le <cyril.phamle@gmail.com>
2018-01-23 22:46:02 +0100

    throw exception if giver already gave score to content

--- src/test/java/fr/knowledge/domain/scores/handlers/AddScoreCommandHandlerTest.java
+++ src/test/java/fr/knowledge/domain/scores/handlers/AddScoreCommandHandlerTest.java
@@ -8,6 +8,7 @@ import fr.knowledge.domain.scores.Mark;
 import fr.knowledge.domain.scores.aggregates.Score;
 import fr.knowledge.domain.scores.commands.AddScoreCommand;
 import fr.knowledge.domain.scores.events.ScoreCreatedEvent;
+import fr.knowledge.domain.scores.exceptions.AlreadyGivenScoreException;
 import fr.knowledge.domain.scores.ports.ScoreRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +18,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -44,5 +47,18 @@ public class AddScoreCommandHandlerTest {
     Score score = Score.of("aaa", "john@doe.fr", ContentType.CATEGORY, "aaa", Mark.FIVE);
     score.saveChanges(new ScoreCreatedEvent(Id.of("aaa"), Username.from("john@doe.fr"), ContentType.CATEGORY, Id.of("aaa"), Mark.FIVE));
     verify(scoreRepository).save(score);
+  }
+
+  @Test
+  public void should_throw_exception_if_giver_already_gave_score_to_content() throws Exception {
+    given(scoreRepository.get(Username.from("john@doe.fr"), Id.of("aaa"))).willReturn(Optional.of(Score.of("aaa", "john@doe.fr", ContentType.CATEGORY, "aaa", Mark.FIVE)));
+    AddScoreCommand command = new AddScoreCommand("john@doe.fr", ContentType.CATEGORY, "aaa", Mark.FIVE);
+
+    try {
+      addScoreCommandHandler.handle(command);
+      fail("Should have thrown AlreadyGivenScoreException.");
+    } catch (AlreadyGivenScoreException e) {
+      assertThat(e).isNotNull();
+    }
   }
 }
